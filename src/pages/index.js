c3fff76573aff18895fc44f8dd54d7e4dcb8e8d7
EmFor2001 <k.hajduk.kh@gmail.com>
2022-02-15 21:19:14 +0100

    test path 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import overLayPic from '../assets/images/overlay.png';
 // import { Link } from 'gatsby';
 
 const sections = [
-  { id: 'one', bg: require("../../static/1.jpg") },
+  { id: 'one', bg: require("/Garni/static/1.jpg") },
   { id: 'two', bg: require("../../static/2.jpg") },
   { id: 'three', bg: require("../../static/3.jpg") },
   { id: 'four', bg: require("../../static/4.jpg") },
